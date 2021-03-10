e89b001858e248c1ad76713ebced31608af3d18f
Lucas Gonçalves e Silva <42481259+lucasgoncalvess@users.noreply.github.com>
2021-03-10 11:39:39 -0300

    Add files via upload

---
+++ Semana 2/prog03.py
@@ -0,0 +1,41 @@
+#Python Tutorial for Beginners 3: Integers and Floats - Working with Numeric Data
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2 sem resto
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+num = 3.14
+
+print(num)
+print(type(num)) #mostra classe do numero
+print(round(num)) #arredonda o numero
+
+#verificar condições
+
+num1 = 3
+num2 = 2
+
+print(num1 != num2)
+
+#converter str em numero
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)

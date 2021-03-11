ea1206a3d10fd0cc4bc1f6258e3be8f22adc1b49
Lucas Gonçalves e Silva <42481259+lucasgoncalvess@users.noreply.github.com>
2021-03-11 10:20:52 -0300

    11_03

--- Semana 2/prog04.py
+++ Semana 2/prog04.py
@@ -10,7 +10,50 @@ courses = ['History', 'Math', 'Physics', 'CompSci']
 # courses.insert(0,'Art') #adiciona na posição que você desejar
 
 #adicionar um vetor dentro de outro
-courses_2 = ['Art', 'Education']
-courses.extend(courses_2)
+# courses_2 = ['Art', 'Education']
+# courses.extend(courses_2)
+#
+# print(courses)
 
-print(courses)
+# Mutable
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+
+print(list_1)
+print(list_2)
+
+# list_1[0] = 'Art'
+
+# print(list_1)
+# print(list_2)
+
+
+# Immutable
+# tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+# tuple_2 = tuple_1
+
+# print(tuple_1)
+# print(tuple_2)
+
+# tuple_1[0] = 'Art'
+
+# print(tuple_1)
+# print(tuple_2)
+
+# Sets
+cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+
+print(cs_courses)
+
+
+# Empty Lists
+empty_list = []
+empty_list = list()
+
+# Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
+
+# Empty Sets
+empty_set = {} # This isn't right! It's a dict
+empty_set = set()

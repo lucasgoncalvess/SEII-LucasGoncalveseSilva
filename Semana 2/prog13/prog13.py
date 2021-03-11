af0d9934cae9355d6f1aca412c3df8719f9d7606
Lucas Gonçalves e Silva <42481259+lucasgoncalvess@users.noreply.github.com>
2021-03-11 16:20:30 -0300

    Novos Codigos

--- Semana 2/prog13/prog13.py
+++ Semana 2/prog13/prog13.py
@@ -1,3 +1,35 @@
 #Python Tutorial: CSV Module - How to Read, Parse, and Write CSV Files
 
 #Muito importante para analise de dados
+
+import csv
+#
+# with open('names.csv', 'r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#
+#     for line in csv_reader:
+#         print(line)
+#
+#     # for line in csv_reader:
+#     #     print(line)
+#
+#     # with open('new_names.csv', 'w') as new_file:
+#     #     csv_writer = csv.writer(new_file, delimiter='-')
+#     #
+#     #     for line in csv_reader:
+#     #         csv_writer.writerow(line)
+
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'last_name']
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            del line['email']
+            csv_writer.writerow(line)
